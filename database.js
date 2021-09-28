12472014a394bfbfd88f164270dcdb91e2b8b12b
Don Carter <Don_Carter@insideranken.org>
2021-09-28 16:47:51 -0500

    Class Example

--- database.js
+++ database.js
@@ -1,6 +1,8 @@
 const debug = require('debug')('app:database');
-const { MongoClient: objectId, MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const config = require('config');
+
+const newId = (str) => ObjectId(str);
 
 let _db = null;
 
@@ -11,6 +13,7 @@ async function connect() {
     const dbName = config.get('db.name');
     const client = await MongoClient.connect(dbUrl);
     _db = client.db(dbName);
+    debug('Connected.');
   }
   return _db;
 }
@@ -18,12 +21,24 @@ async function connect() {
 async function ping() {
   const db = await connect();
   await db.command({ ping: 1 });
-  debug('Ping');
+  debug('Ping.');
 }
 
 async function findAllPets() {
   const db = await connect();
-  return db.collection('pets').find({}).toArray();
+  const pets = await db.collection('pets').find({}).toArray();
+  return pets;
+}
+
+async function findPetById(petId) {
+  const db = await connect();
+  const pet = await db.collection('pets').findOne({ _id: { $eq: petId } });
+  return pet;
+}
+
+async function insertOnePet(pet) {
+  const db = await connect();
+  await db.collection('pets').insertOne(pet);
 }
 
 ping();
@@ -31,5 +46,8 @@ ping();
 module.exports = {
   connect,
   ping,
-  findAllPets
+  findAllPets,
+  newId,
+  findPetById,
+  insertOnePet
 };
